257c5a82a7e1ea76ef7cecd3729b34b93bd5c85f
trentransom <116039236+trentransom@users.noreply.github.com>
2023-03-25 11:28:39 -0400

    update dash

--- Trent/app.py
+++ Trent/app.py
@@ -7,76 +7,47 @@ ssl._create_default_https_context = ssl._create_unverified_context
 app = Dash(__name__)
 
 df = pd.read_csv('https://raw.githubusercontent.com/AndrewRBader/CU-Fintech_Project-2/main/Resources/spotify_us_df.csv')
+columns_list = list(df.columns)
 
 app.layout = html.Div([
     html.Div([
 
         html.Div([
             dcc.Dropdown(
-                df['Artist'].unique(),
+                columns_list,
                 'Artist',
                 id='xaxis-column'
-            ),
-            dcc.RadioItems(
-                ['Linear', 'Log'],
-                'Linear',
-                id='xaxis-type',
-                inline=True
             )
         ], style={'width': '48%', 'display': 'inline-block'}),
 
         html.Div([
             dcc.Dropdown(
-                df['Popularity'].unique(),
+                columns_list,
                 'Popularity',
                 id='yaxis-column'
-            ),
-            dcc.RadioItems(
-                ['Linear', 'Log'],
-                'Linear',
-                id='yaxis-type',
-                inline=True
             )
         ], style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
     ]),
 
     dcc.Graph(id='indicator-graphic'),
-
-    dcc.Slider(
-        df['Popu_max'].min(),
-        df['Popu_max'].max(),
-        step=None,
-        id='popu_slider',
-        value=df['Popu_max'].max(),
-        marks={str(popularity): str(popularity) for popularity in df['Popu_max'].unique()},
-
-    )
 ])
 
 
 @app.callback(
     Output('indicator-graphic', 'figure'),
     Input('xaxis-column', 'value'),
-    Input('yaxis-column', 'value'),
-    Input('xaxis-type', 'value'),
-    Input('yaxis-type', 'value'),
-    Input('popu_slider', 'value'))
-def update_graph(xaxis_column_name, yaxis_column_name,
-                 xaxis_type, yaxis_type,
-                 popularity_value):
-    dff = df[df['Year'] == popularity_value]
+    Input('yaxis-column', 'value')
+)
+    
+def update_graph(xaxis_column_name, yaxis_column_name):
+    dff = df
 
-    fig = px.scatter(x=dff[dff['Indicator Name'] == xaxis_column_name]['Value'],
-                     y=dff[dff['Indicator Name'] == yaxis_column_name]['Value'],
-                     hover_name=dff[dff['Indicator Name'] == yaxis_column_name]['Title'])
+    fig = px.scatter(x=dff[xaxis_column_name],
+                     y=dff[yaxis_column_name]
+                     )
 
     fig.update_layout(margin={'l': 40, 'b': 40, 't': 10, 'r': 0}, hovermode='closest')
 
-    fig.update_xaxes(title=xaxis_column_name,
-                     type='linear' if xaxis_type == 'Linear' else 'log')
-
-    fig.update_yaxes(title=yaxis_column_name,
-                     type='linear' if yaxis_type == 'Linear' else 'log')
 
     return fig
 
